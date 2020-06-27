7f3f231e851e84b3e3cc630c1b83f40c4bcd9e9c
Kishor Sutar <kishor007sutar@gmail.com>
2020-06-27 23:25:31 +0100

    Added Hamcrest library for more readable tests

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsKtTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsKtTest.kt
@@ -1,6 +1,7 @@
 package com.example.android.architecture.blueprints.todoapp.statistics
 
 import com.example.android.architecture.blueprints.todoapp.data.Task
+import org.hamcrest.Matchers.`is`
 import org.junit.Assert.*
 import org.junit.Test
 
@@ -12,7 +13,23 @@ class StatisticsUtilsKtTest {
                 Task("title", "desc", isCompleted = false)
         )
         val result = getActiveAndCompletedStats(tasks)
-        assertEquals(result.completedTasksPercent, 0f)
-        assertEquals(result.activeTasksPercent, 100f)
+        assertThat(result.completedTasksPercent, `is`(0f))
+        assertThat(result.activeTasksPercent, `is`(100f))
+    }
+
+    @Test
+    fun `given active and completed tasks when there is no active tasks then returns zero and hundred`() {
+        val tasks = listOf(
+                Task("Title", "desc", isCompleted = true)
+        )
+        val result = getActiveAndCompletedStats(tasks)
+        assertThat(result.activeTasksPercent, `is`(0f))
+        assertThat(result.completedTasksPercent, `is`(100f))
+    }
+
+    // Hamcrest testing method using Given, when , then
+    @Test
+    fun `given the tests are passing return true`() {
+        assertTrue(true)
     }
 }
