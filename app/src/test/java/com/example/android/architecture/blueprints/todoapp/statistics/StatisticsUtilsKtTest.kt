896c2735a0d5f6a6f9f3007cae1874a573dbc7be
Kishor Sutar <kishor007sutar@gmail.com>
2020-06-27 23:40:49 +0100

    Fixed null and empty stats issue

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsKtTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsKtTest.kt
@@ -27,6 +27,35 @@ class StatisticsUtilsKtTest {
         assertThat(result.completedTasksPercent, `is`(100f))
     }
 
+    @Test
+    fun `given 3 completed tasks and 2 active tasks when getActiveAndCompletedStats called then return forty sixty`() {
+        val tasks = listOf(
+                Task("title", "desc", isCompleted = true),
+                Task("title", "desc", isCompleted = true),
+                Task("title", "desc", isCompleted = true),
+                Task("title", "desc", isCompleted = false),
+                Task("title", "desc", isCompleted = false)
+        )
+
+        val result = getActiveAndCompletedStats(tasks)
+        assertThat(result.activeTasksPercent, `is`(40f))
+        assertThat(result.completedTasksPercent, `is`(60f))
+    }
+
+    @Test
+    fun `given when there's an error loading stats both active and completed are zero`() {
+        val result = getActiveAndCompletedStats(null)
+        assertThat(result.completedTasksPercent, `is`(0f))
+        assertThat(result.activeTasksPercent, `is`(0f))
+    }
+
+    @Test
+    fun `given when there's an empty stats both active and completed are zero`() {
+        val result = getActiveAndCompletedStats(emptyList())
+        assertThat(result.completedTasksPercent, `is`(0f))
+        assertThat(result.activeTasksPercent, `is`(0f))
+    }
+
     // Hamcrest testing method using Given, when , then
     @Test
     fun `given the tests are passing return true`() {
